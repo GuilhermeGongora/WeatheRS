9a0e4e17230a33dc44ea3a8066f6f957afb28b01
Guilherme Gongora <guilhermegongora2007@gmail.com>
2024-08-08 23:37:48 -0300

    Class rename

--- WeatherApp/WeatherApp/Models/WeatherObj.cs
+++ WeatherApp/WeatherApp/Models/WeatherObj.cs
@@ -1,9 +1,8 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace CompleteWeatherApp.Models
+namespace WeatherApp.Models
 {
     public class ForecastInfo
     {
