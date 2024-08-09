3217c3734c1110d0fd5f36f8605fc1c8322696d8
Guilherme Gongora <guilhermegongora2007@gmail.com>
2024-08-08 23:42:48 -0300

    GetWeatherMethod

--- WeatherApp/WeatherApp/Views/HomePage.xaml.cs
+++ WeatherApp/WeatherApp/Views/HomePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherApp.Helper;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,6 +18,13 @@ namespace WeatherApp.Views
         }
 
         private string Location = "France";
+
+        private async void GetWeatherInfo()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid=b6907d289e10d714a6e88b30761fae22&units=metric";
+
+            var result = await ApiCaller.Get(url);
+        }
        
         }
     }
