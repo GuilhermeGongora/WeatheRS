3a72ebaf1998d306dd34debb590576ee15ad935e
Guilherme Gongora <guilhermegongora2007@gmail.com>
2024-08-09 00:02:43 -0300

    New forecast method

--- WeatherApp/WeatherApp/Views/HomePage.xaml.cs
+++ WeatherApp/WeatherApp/Views/HomePage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WeatherApp.Helper;
+using WeatherApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,15 +17,48 @@ namespace WeatherApp.Views
         public HomePage()
         {
             InitializeComponent();
+            GetWeatherInfo();
         }
 
         private string Location = "France";
 
         private async void GetWeatherInfo()
         {
-            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid=b6907d289e10d714a6e88b30761fae22&units=metric";
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={Location}&appid=0254e13028fbf335e64c91ff361ce46f&units=metric";
 
             var result = await ApiCaller.Get(url);
+            if(result.Successful)
+            {
+                try
+                {
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+                    descriptionTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    iconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+                    humidityTxt.Text = $"{weatherInfo.main.humidity}%";
+                    pressureTxt.Text = $"{weatherInfo.main.pressure} hpa";
+                    windTxt.Text = $"{weatherInfo.wind.speed} m/s";
+                    cloudinessTxt.Text = $"{weatherInfo.clouds.all}%";
+
+                    var dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            else
+            {
+                await DisplayAlert("Weather Info", "No weather information found", "OK");
+            }
+        }
+
+        private async void GetForecast()
+        {
+
         }
        
         }
