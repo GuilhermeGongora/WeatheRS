0d321cf20ef6b4de1392d05e095e55064d314d34
Guilherme Gongora <guilhermegongora2007@gmail.com>
2024-09-02 20:10:15 -0300

    New flyout approach

---
+++ WeatherApp/WeatherApp/Views/FlyoutPageMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Views
+{
+    class FlyoutItemPage
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type targetPage { get; set; }
+    }
+}
