1de2bab97fca57d0f4007b2462c68c931bacff07
Guilherme Gongora <guilhermegongora2007@gmail.com>
2024-10-10 22:32:51 -0300

    new images icon

---
+++ WeatherApp/WeatherApp/Views/RsDonationPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RsDonationPage : ContentPage
+	{
+		public RsDonationPage ()
+		{
+			InitializeComponent ();
+		}
+        private async void OnImageButtonClicked(object sender, EventArgs e)
+        {
+            var button = (ImageButton)sender;
+            var instagramUrl = (string)button.CommandParameter; // Obtém o URL do CommandParameter
+
+            await Launcher.OpenAsync(instagramUrl);
+        }
+
+    }
+}
